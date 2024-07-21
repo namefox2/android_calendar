c8f97b5b804cbfb62e4e1f0f6e292bf4aa1e02fa
seonae <seonae_95@naver.com>
2024-07-21 23:55:58 +0900

    main_activity.xml

--- app/src/main/java/com/example/calendar/MainActivity.java
+++ app/src/main/java/com/example/calendar/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.calendar;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -12,12 +17,44 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        TextView toolbarTitle = findViewById(R.id.toolbar_title);
+        String toolbar_text = "TODO";
+        toolbarTitle.setText(toolbar_text);
+
+        if(getSupportActionBar() != null)
+        {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+//        androidx.appcompat.app.ActionBar actionBar = getSupportActionBar();
+//        if(actionBar != null)
+//        {
+//            actionBar.setTitle("YOU CAN DO IT");
+//            actionBar.setDisplayHomeAsUpEnabled(true);
+//        }
     }
 
-    public boolean onCreateOptionMenu(Menu menu)
+    public boolean onCreateOptionsMenu(Menu menu)
     {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.actionbar_menu, menu);
         return true;
+    }
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+        if(id == R.id.bar_success_percent)
+        {
+
+            return true;
+        }
+        else if(id == R.id.bar_random_list)
+        {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
